aa4a678f747131bfa65abd46359bb4d576caf7f6
nidhi2793 <MY_NAME@example.com>
2023-11-10 22:00:48 +0530

    login sign up for multi users

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -11,10 +11,7 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useNavigate } from "react-router-dom";
-
-const defaultTheme = createTheme();
 
 export default function Login() {
   const navigate = useNavigate();
@@ -22,22 +19,34 @@ export default function Login() {
   const [enteredEmail, setEmail] = useState("");
   const [enteredPass, setPass] = useState("");
 
+  //submitting login form
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      if (localStorage.getItem(enteredEmail) == null) {
+      let users = JSON.parse(localStorage.getItem("users"));
+      let user;
+
+      //checking if email id is registered
+      if (users.length > 0) {
+        let indexOfUser = users.findIndex((i) => i.email === enteredEmail);
+        user = users[indexOfUser];
+      }
+      if (users.length === 0 || !user) {
         toast("Email id is not registered", {
           type: "error",
           theme: "colored",
         });
       } else {
-        let password = JSON.parse(localStorage.getItem(enteredEmail)).password;
-        if (enteredPass != password) {
-          toast("Please enter correct password", {
+        //matching entered passwors with registered user's password
+        let correctPassword = user.password;
+        if (enteredPass !== correctPassword) {
+          toast("Invalid Credentials", {
             type: "error",
             theme: "colored",
           });
         } else {
+          //storing user as logged in
+          localStorage.setItem("loggedIn", JSON.stringify(enteredEmail));
           navigate("/home");
         }
       }
@@ -48,79 +57,76 @@ export default function Login() {
 
   return (
     <>
-      <ThemeProvider theme={defaultTheme}>
-        <Container component="main" maxWidth="xs">
-          <CssBaseline />
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: 3,
+            borderRadius: 10,
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "#ef79d9" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Log in
+          </Typography>
           <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              backgroundColor: "white",
-              padding: 3,
-              borderRadius: 10,
-            }}
+            component="form"
+            onSubmit={handleSubmit}
+            sx={{ mt: 1, height: 150 }}
           >
-            <Avatar sx={{ m: 1, bgcolor: "#ef79d9" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Log in
-            </Typography>
-            <Box
-              component="form"
-              onSubmit={handleSubmit}
-              sx={{ mt: 1, height: 150 }}
+            <TextField
+              type={"email"}
+              slotProps={{ input: { type: "email" } }}
+              margin="dense"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              size="small"
+              value={enteredEmail}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              margin="dense"
+              required
+              fullWidth
+              name="password"
+              label="Password (min 6 characters)"
+              type="password"
+              id="password"
+              size="small"
+              value={enteredPass}
+              onChange={(e) => setPass(e.target.value)}
+            />
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
             >
-              <TextField
-                type={"email"}
-                slotProps={{ input: { type: "email" } }}
-                margin="dense"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                size="small"
-                value={enteredEmail}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <TextField
-                margin="dense"
-                required
-                fullWidth
-                name="password"
-                label="Password (min 6 characters)"
-                type="password"
-                id="password"
-                size="small"
-                value={enteredPass}
-                onChange={(e) => setPass(e.target.value)}
-              />
+              Login
+            </Button>
 
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Login
-              </Button>
-
-              <Grid container>
-                <Grid item>
-                  <Link href="/" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
+            <Grid container>
+              <Grid item>
+                <Link href="/" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
               </Grid>
-            </Box>
+            </Grid>
           </Box>
-        </Container>
-      </ThemeProvider>
+        </Box>
+      </Container>
       <ToastContainer />
     </>
   );
--- src/components/authentication/SignUp.js
+++ src/components/authentication/SignUp.js
@@ -10,15 +10,13 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import MenuItem from "@mui/material/MenuItem";
-import { MuiTelInput } from "mui-tel-input";
+import { MuiTelInput, matchIsValidTel } from "mui-tel-input";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useNavigate } from "react-router-dom";
 
-const defaultTheme = createTheme();
-
 export default function SignUp() {
+  //array for profession dropdown
   const professions = [
     { id: 1, title: "Lawyer" },
     { id: 2, title: "Engineer" },
@@ -37,176 +35,199 @@ export default function SignUp() {
   const [enteredPass, setPass] = useState("");
   const [enteredPhone, setPhone] = useState("");
 
+  //managing profession
   const handleProfession = (event) => {
     setProfession(event.target.value);
   };
 
+  //managing submission og sign up form
   const handleSubmit = async (event) => {
     event.preventDefault();
 
+    //validating phone number
+    if (matchIsValidTel(enteredPhone) === false) {
+      toast("Enter a valid phone number", {
+        type: "error",
+        theme: "colored",
+      });
+      return;
+    }
+
+    //validating email id
     function validateEmail(email) {
       var re = /\S+@\S+\.\S+/;
       return re.test(email);
     }
-
-    console.log(validateEmail(enteredEmail));
-    if (validateEmail(enteredEmail) == false) {
+    if (validateEmail(enteredEmail) === false) {
       toast("Enter a valid email id", {
         type: "error",
         theme: "colored",
       });
-    } else if (enteredPass.length < 6) {
+      return;
+    }
+
+    //checking for password length
+    if (enteredPass.length < 6) {
       toast("Password should of atleast 6 characters", {
         type: "error",
         theme: "colored",
       });
-    } else {
-      try {
-        let userData = {
-          username: enteredName,
-          email: enteredEmail,
-          password: enteredPass,
-          phone: enteredPhone,
-          profession,
-        };
-        if (localStorage.getItem(enteredEmail) != null) {
-          toast("Email id already registered", {
-            type: "error",
-            theme: "colored",
-          });
-        } else {
-          localStorage.setItem(enteredEmail, JSON.stringify(userData));
-          toast("User Registered Successfully !", {
-            type: "success",
-            theme: "colored",
-          });
-          setTimeout(() => {
-            navigate("/login");
-          }, 1000);
-        }
-      } catch (error) {
-        console.log(error);
+      return;
+    }
+
+    try {
+      let user;
+      let userData = {
+        username: enteredName,
+        email: enteredEmail,
+        password: enteredPass,
+        phone: enteredPhone,
+        profession,
+      };
+      let users = JSON.parse(localStorage.getItem("users") || "[]");
+
+      //checking if user already exists; if not creating account
+      //and navigating to login page
+
+      if (users.length > 0) {
+        let indexOfUser = users.findIndex((i) => i.email === enteredEmail);
+        user = users[indexOfUser];
       }
+      if (user) {
+        toast("Email address already registered.", {
+          type: "error",
+          theme: "colored",
+        });
+      } else {
+        users.push(userData);
+        localStorage.setItem("users", JSON.stringify(users));
+        toast("User registered", {
+          type: "success",
+          theme: "colored",
+        });
+        setTimeout(() => {
+          navigate("/login");
+        }, 1000);
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
   return (
     <>
-      <ThemeProvider theme={defaultTheme}>
-        <Container component="main" maxWidth="xs">
-          <CssBaseline />
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: 3,
+            borderRadius: 10,
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "#FFC436" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
           <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              backgroundColor: "white",
-              padding: 3,
-              borderRadius: 10,
-            }}
+            component="form"
+            onSubmit={handleSubmit}
+            sx={{ mt: 1, height: 150 }}
           >
-            <Avatar sx={{ m: 1, bgcolor: "#FFC436" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign up
-            </Typography>
-            <Box
-              component="form"
-              onSubmit={handleSubmit}
-              sx={{ mt: 1, height: 150 }}
+            <TextField
+              margin="dense"
+              required
+              fullWidth
+              id="name"
+              label="Name"
+              name="name"
+              value={enteredName}
+              onChange={(e) => setName(e.target.value)}
+              autoFocus
+              size="small"
+            />
+            <TextField
+              type={"email"}
+              slotProps={{ input: { type: "email" } }}
+              margin="dense"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              size="small"
+              value={enteredEmail}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              margin="dense"
+              required
+              fullWidth
+              name="password"
+              label="Password (min 6 characters)"
+              type="password"
+              id="password"
+              size="small"
+              value={enteredPass}
+              onChange={(e) => setPass(e.target.value)}
+            />
+            <MuiTelInput
+              margin="dense"
+              required
+              fullWidth
+              name="phone"
+              label="Phone Number"
+              type="phone"
+              id="phone"
+              size="small"
+              value={enteredPhone}
+              onChange={(newValue) => setPhone(newValue)}
+            />
+            <TextField
+              required
+              size="small"
+              margin="dense"
+              fullWidth
+              variant="outlined"
+              value={profession}
+              select
+              label="Profession"
+              onChange={handleProfession}
+              sx={{ textAlign: "left" }}
             >
-              <TextField
-                margin="dense"
-                required
-                fullWidth
-                id="name"
-                label="Name"
-                name="name"
-                value={enteredName}
-                onChange={(e) => setName(e.target.value)}
-                autoFocus
-                size="small"
-              />
-              <TextField
-                type={"email"}
-                slotProps={{ input: { type: "email" } }}
-                margin="dense"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                size="small"
-                value={enteredEmail}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <TextField
-                margin="dense"
-                required
-                fullWidth
-                name="password"
-                label="Password (min 6 characters)"
-                type="password"
-                id="password"
-                size="small"
-                value={enteredPass}
-                onChange={(e) => setPass(e.target.value)}
-              />
-              <MuiTelInput
-                margin="dense"
-                required
-                fullWidth
-                name="phone"
-                label="Phone Number"
-                type="phone"
-                id="phone"
-                size="small"
-                value={enteredPhone}
-                onChange={(newValue) => setPhone(newValue)}
-              />
-              <TextField
-                required
-                size="small"
-                margin="dense"
-                fullWidth
-                variant="outlined"
-                value={profession}
-                select
-                label="Profession"
-                onChange={handleProfession}
-                sx={{ textAlign: "left" }}
-              >
-                {professions.map((prof) => (
-                  <MenuItem value={prof.title} key={prof.id}>
-                    {prof.title}
-                  </MenuItem>
-                ))}
-              </TextField>
+              {professions.map((prof) => (
+                <MenuItem value={prof.title} key={prof.id}>
+                  {prof.title}
+                </MenuItem>
+              ))}
+            </TextField>
 
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                SignUp
-              </Button>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              SignUp
+            </Button>
 
-              <Grid container>
-                <Grid item>
-                  <Link href="/login" variant="body2">
-                    {"Already have an account? Sign In"}
-                  </Link>
-                </Grid>
+            <Grid container>
+              <Grid item>
+                <Link href="/login" variant="body2">
+                  {"Already have an account? Sign In"}
+                </Link>
               </Grid>
-            </Box>
+            </Grid>
           </Box>
-        </Container>
-      </ThemeProvider>
+        </Box>
+      </Container>
       <ToastContainer />
     </>
   );
